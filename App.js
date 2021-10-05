271bee3e4a0cc7d65af1ede4fe71830bccf65d6c
KayoSilva19 <Kayohenriquesilva19@gmail.com>
2021-10-04 23:43:03 -0300

    appDeAnotacoes

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator();
 export default function app() {
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Lodin">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={Login}
